7f097d9f5801a3387bed4559a05090710c23f005
Shivam <sriyanshshivam1@gmail.com>
2025-03-11 01:44:37 +0530

    Complete image job.go

---
+++ models/image.go
@@ -0,0 +1,10 @@
+package models
+
+type Image struct {
+    ID        string  `bson:"_id,omitempty"`
+    JobID     string  `bson:"job_id"`
+    StoreID   string  `bson:"store_id"`
+    ImageURL  string  `bson:"image_url"`
+    Perimeter int     `bson:"perimeter,omitempty"`
+    Status    string  `bson:"status"`
+}
