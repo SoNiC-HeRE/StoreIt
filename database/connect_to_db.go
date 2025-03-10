970a4ec37913aa6743f7890a0923551cbd94e207
Shivam <sriyanshshivam1@gmail.com>
2025-03-11 01:43:13 +0530

    Complete connDb.go

---
+++ database/connect_to_db.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+    "context"
+    "log"
+    "retail_pulse/config"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var JobCollection *mongo.Collection
+var ImageCollection *mongo.Collection
+
+func ConnectMongoDB() {
+    clientOptions := options.Client().ApplyURI(config.GetMongoURI())
+    client, err := mongo.Connect(context.TODO(), clientOptions)
+    if err != nil {
+        log.Fatal("MongoDB connection failed:", err)
+    }
+
+    database := client.Database("retail_pulse")
+    JobCollection = database.Collection("jobs")
+    ImageCollection = database.Collection("images")
+
+    log.Println("Connected to MongoDB")
+}
